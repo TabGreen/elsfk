f93b6ac1022bd3ad86aecf1205a520a91db6655c
TabGreen <141554864+TabGreen@users.noreply.github.com>
2024-08-20 14:21:06 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,7 +9,8 @@ const colors = [
     "#FF0000",
     "#0000FF",
     "#800080"
-];
+];const borderOfBlock = 0.2;//边框方块的百分比
+const colorChange = [0.6,0.05,0.05];//方块周围的梯形颜色增减值的数组
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var gameCVS = document.getElementsByTagName('canvas')[0];
 var gameCTX = gameCVS.getContext("2d");
@@ -33,3 +34,61 @@ function drawBlock(ctx,x, y, color){
     ctx.strokeStyle = color;
     ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
 }drawBlock(gameCTX,0,0,colors[0]);
+drawBlock(gameCTX,1,0,colors[1]);
+function drawBlock(ctx,x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect((x+borderOfBlock) * blockSize, (y+borderOfBlock) * blockSize, blockSize *(1-borderOfBlock*2), blockSize * (1-borderOfBlock*2));
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    //绘制第一个梯形
+    gameCTX.beginPath();
+    gameCTX.moveTo(x * blockSize, y * blockSize);
+    gameCTX.lineTo((x+1)* blockSize, y * blockSize);
+    gameCTX.lineTo((x+1-borderOfBlock) * blockSize, (y+borderOfBlock) * blockSize);
+    gameCTX.lineTo((x+borderOfBlock) * blockSize, (y+borderOfBlock) * blockSize);
+    gameCTX.closePath();
+    gameCTX.strokeStyle = adjustColorBrightness(color,colorChange[0]);
+    gameCTX.fillStyle = adjustColorBrightness(color,colorChange[0]);
+    gameCTX.fill();
+    gameCTX.stroke();
+
+}
+
+function adjustColorBrightness(hexColor, percent) {//改变颜色亮度,使方块更灵动
+    var r = parseInt(hexColor.substring(1, 3), 16);
+    var g = parseInt(hexColor.substring(3, 5), 16);
+    var b = parseInt(hexColor.substring(5, 7), 16);
+    var color = [r,g,b];
+    if(percent > 0){
+        for(let i = 0;i<color.length;i++){
+            if(color[i]>=255){
+                continue;
+            }if(color[i]<=0){
+                color[i]=Math.floor(255*percent);
+                continue;
+            }color[i] += color[i] * percent;
+            if(color[i]>255){
+                color[i]=255;
+            }
+        }
+    }if(percent<0){
+        percent = Math.abs(percent);
+        for(let i = 0;i<color.length;i++){
+            if(color[i]<=0){
+                continue;
+            }if(color[i]>=255){
+                color[i]=Math.floor(255* percent);
+                continue;
+            }
+            color[i] -= color[i] * percent;
+            if(color[i]<=0){
+                color[i]=0;
+            }
+        }
+    }
+    var newColor =   `#${color[0].toString(16).padStart(2, '0')}${color[1].toString(16).padStart(2, '0')}${color[2].toString(16).padStart(2, '0')}`;
+    
+    console.log(r,g,b);
+    console.log(newColor)
+    return newColor;
+}
