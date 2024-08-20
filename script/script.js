ddde11373e176ac4a087ef0fce6e80e23844877f
TabGreen <141554864+TabGreen@users.noreply.github.com>
2024-08-20 12:00:44 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 const blockSize = 25;//单个方块的大小25像素
 const heigh = 20;//y轴方向方块个数最多20个
 const width = 10;//x轴方向方块个数最多10个
-
-var gameCVS = document.getElementById("gameCVS");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var gameCVS = document.getElementsByTagName('canvas')[0];
 var gameCTX = gameCVS.getContext("2d");
 
 var bufferCVS = document.createElement("canvas");
@@ -14,3 +14,14 @@ function setGameCVSSize(){
     bufferCVS.width = gameCVS.width;
     bufferCVS.height = gameCVS.height;
 }setGameCVSSize();
+
+function update(){
+
+}
+function drawBlock(ctx,x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+}
+drawBlock(gameCTX,0,0,'#00ff00');
